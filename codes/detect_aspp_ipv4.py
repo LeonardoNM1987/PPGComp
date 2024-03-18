7312d8e53d522d5dac49d18d51f57f0ff5caf85a
LeonardoNM1987 <leonardonm1987@gmail.com>
2024-03-18 13:51:43 -0300

    backup

--- codes/detect_aspp_ipv4.py
+++ codes/detect_aspp_ipv4.py
@@ -1,7 +1,23 @@
 import datetime
 import os
 
-         
+
+prepNivel1 = []                         # listar prepends de nivel 1
+prepNivel2 = []                         # listar prepends de nivel 2
+prepNivel3 = []                         # listar prepends de nivel 3
+prepNivel4plus = []                     # listar prepends de nivel 4 ou superior
+prependOrigem = []                      # listar prepends de prependOrigem
+prependIntermed = []                    # listar prepends prependIntermediarios
+prependTotais = 0                       # quantitativo de visualizações de prepend em geral(apenas debug)    
+vizinhos = {}                           # listar todos vizinhos que cada ASN teve
+asesTotais = []                         # listar todos ASes unicos vistos na análise
+asPathUnico = set()                     # conjunto para armazenar AS_PATHs únicos e eliminar rotas duplicadas
+rotasBogon = []                         # identificacao das rotas bogon para debug
+rotasRemovidas = 0                      # identificacao das rotas bogon para debug
+rotaValida = 0                          # identificacao das rotas validas para debug
+numLinha = 0
+
+
 ################################################ FUNCOES ################################################
 
 def listaVizinhos(dicionario, chave, valor):                           # Aqui a funcao analisa quem é vizinho de quem
@@ -38,15 +54,38 @@ def contaPrepend(aspath):
                     trigger = False
             i+=1
 
-def contabilizar_duplicidade(as_path):
-    as_contagem = {}
-    as_set = set()    
-    for asn in as_path:
-        if asn in as_set:
-            as_contagem[asn] = as_contagem.get(asn, 0) + 1
+            
+def identificar_prepend_nivel(as_path):
+    """
+    Identifica o nível de AS-Path prepend para cada ASN repetido no AS-Path fornecido,
+    organizando-os em listas por nível de prepend.
+    """
+    # Dicionário temporário para contar os prepends
+    nivel_prepend_temp = {}
+    contador_temp = 1  # Contador para acompanhar repetições consecutivas
+
+    for i in range(1, len(as_path)):
+        if as_path[i] == as_path[i-1]:
+            contador_temp += 1
         else:
-            as_set.add(asn)    
-    return as_contagem
+            if contador_temp > 1:
+                nivel_prepend_temp[as_path[i-1]] = contador_temp - 1
+            contador_temp = 1
+
+    if contador_temp > 1:
+        nivel_prepend_temp[as_path[-1]] = contador_temp - 1
+
+    # Organiza os ASNs em listas por nível de prepend
+    #prepNivel1, prepNivel2, prepNivel3, prepNivel4plus = [], [], [], []
+    for asn, nivel in nivel_prepend_temp.items():
+        if nivel == 1:
+            prepNivel1.append(asn)
+        elif nivel == 2:
+            prepNivel2.append(asn)
+        elif nivel == 3:
+            prepNivel3.append(asn)
+        else:
+            prepNivel4plus.append(asn)
 
 
 ################################################ INPUTS #################################################
@@ -55,40 +94,31 @@ bogonsList = './codes/bogonsList.txt'                                  # lista d
 #source = 'validadores/validador_IPv4_01.txt'                           # ARQUIVO PARA VALIDAÇÃO
 #source = 'source/rib.20200320.0000.txt'                  # ARQUIVO PARA ANÁLISE
 
-# sourceBatch = [
-#     'source/rib.20110320.0000.txt',
-#     'source/rib.20130320.0000.txt',
-#     'source/rib.20150320.0000.txt',
-#     'source/rib.20170320.0000.txt',
-#     'source/rib.20200320.0000.txt'    
-# ]
+# lote de snapshots para análise sequencial
+sourceBatch = [
+    'source/rib.20110320.0000.txt',
+    'source/rib.20130320.0000.txt',
+    'source/rib.20150320.0000.txt',
+    'source/rib.20170320.0000.txt',
+    'source/rib.20200320.0000.txt'    
+]
 # sourceBatch = ['validadores/validador_IPv4_01.txt',
 #                'validadores/validador_IPv4_02.txt', 
 #                'validadores/validador_IPv4_03.txt']
-sourceBatch = ['validadores/validador_IPv4_01.txt']
+#sourceBatch = ['validadores/validador_IPv4_01.txt']
 
 ################################################ EXECUÇÃO #################################################
 
-for snapshot in sourceBatch:                                               #esse for replica diversas analises sequenciais
+for snapshot in sourceBatch:                                               # esse for analisa snapshots em lote
 
     inicio = datetime.datetime.now()                                       # Marca o início da execução
 
-    prependOrigem = []                                                     # contabiliza prepends de prependOrigem
-    prependIntermed = []                                                   # contabiliza prepends prependIntermediarios
-    prependTotais = 0                                                      # contabiliza visualizações de prepend em geral(apenas debug)
-    vizinhos = {}                                                          # contabiliza todos vizinhos que cada ASN teve
-    asesTotais = []                                                        # contabiliza todos ASes unicos vistos na análise
-    asPathUnico = set()                                                    # conjunto para armazenar AS_PATHs únicos
-    rotasBogon = []
-    rotasRemovidas = 0
-    rotaValida = 0
-    numLinha = 0
-    
+       
     with open(bogonsList, 'r') as arquivoBogon:                              #abertura e conversão do arquivo bogon para uma lista
         for linha in arquivoBogon.readlines():
             rotasBogon.append(linha.strip())
 
-    print('ANALISANDO SNAPSHOT...')
+    print(f'ANALISANDO SNAPSHOT: {snapshot}......')
     with open(snapshot, 'r') as arquivo:    
                                                                   
         for linha in arquivo:                                               #ler linha por linha    
@@ -104,7 +134,7 @@ for snapshot in sourceBatch:                                               #esse
                         if as_path_str not in asPathUnico:                 #Verifica se o AS_PATH é único(evita contar rotas duplicadas em coletores diferentes)
                             asPathUnico.add(as_path_str)                   #Adiciona o AS_PATH ao conjunto de únicos
                             contaPrepend(asPath)                           # >>>>>>>>> AQUI PODE SER UTIL USAR HPC
-                            #contabilizar_duplicidade(asPath)                  # >>>>>>>>> AQUI PODE SER UTIL USAR HPC
+                            identificar_prepend_nivel(asPath)               # >>>>>>>>> AQUI PODE SER UTIL USAR HPC
                             asn = asPath                                   #quebra o path em vários asn 
                             for i, num in enumerate(asn):                  #Iterar pela lista de ASes
                                 if i > 0:                                  #Certificar de não ser o primeiro elemento
@@ -131,8 +161,12 @@ for snapshot in sourceBatch:                                               #esse
         arquivoResultados.write(f'{prependTotais}\n')               # 1 - Total de prepends contabilizados
         arquivoResultados.write(f'{prependOrigem}\n')               # 2 - Lista de ASes com prepend na prependOrigem
         arquivoResultados.write(f'{prependIntermed}\n')             # 3 - Lista de ASes com prepend prependIntermediario
-        arquivoResultados.write(f'{asesTotais}\n')                  # 4 - Lista de ASes unicos visualizados
-        arquivoResultados.write(f'{vizinhos}\n')                    # 5 - ASes com seus respctivos vizinhos listados
+        arquivoResultados.write(f'{prepNivel1}\n')                  # 4 - Lista de ASes que fazem prepend nivel 1
+        arquivoResultados.write(f'{prepNivel2}\n')                  # 5 - Lista de ASes que fazem prepend nivel 2
+        arquivoResultados.write(f'{prepNivel3}\n')                  # 6 - Lista de ASes que fazem prepend nivel 3
+        arquivoResultados.write(f'{prepNivel4plus}\n')              # 7 - Lista de ASes que fazem prepend nivel 4 ou +
+        arquivoResultados.write(f'{asesTotais}\n')                  # 8 - Lista de ASes unicos visualizados
+        arquivoResultados.write(f'{vizinhos}\n')                    # 9 - ASes com seus respectivos vizinhos listados
     print(f"Os resultados foram salvos em: {output}")
 
     #duplicados_contagem = contabilizar_duplicidade(asPath)
@@ -146,8 +180,10 @@ for snapshot in sourceBatch:                                               #esse
     print('Análise concluída com sucesso!')
     print(f'Rotas IPv4 inválidas e ignoradas: {rotasRemovidas}') 
     print(f'Rotas IPv4 analisadas: {rotaValida}')
-    #print(f"ASes com prepend: {duplicados_contagem}")
+    #print(f"ASes com prepend: {contabilizar_duplicidade}")    
     print(f"Tempo de execução: {tempo_formatado}")
+
+    
 
 
 
@@ -164,3 +200,18 @@ for snapshot in sourceBatch:                                               #esse
 # vizinhos_formatados = {k: list(v) for k, v in vizinhos.items()} # Convertendo conjuntos para listas para melhor visualização
 # print(f'Lista de ASN e seus respectivos vizinhos: {vizinhos}\n')
 # print(f'rotaValida no arquivo: {rotaValida}') 
+    
+
+
+
+
+    
+# def contabilizar_duplicidade(as_path):    
+#     as_contagem = {}
+#     as_set = set()    
+#     for asn in as_path:
+#         if asn in as_set:
+#             as_contagem[asn] = as_contagem.get(asn, 0) + 1
+#         else:
+#             as_set.add(asn)
+#     return as_contagem
--- results/README.txt
+++ results/README.txt
@@ -1,9 +1,13 @@
 Nesta pasta ficarão os resultados das análises, antes de plotar
 
+Estrutura dos arquivos de resultados:
 
-Ordem das linhas do arquivo results: 
-1 - Total de prepends contabilizados
-2 - Lista de ASes com prepend na origem
-3 - Lista de ASes com prepend intermediario
-4 - Lista de ASes unicos visualizados
-5 - ASes com seus respectivos vizinhos listados
+# 1 - Total de prepends contabilizados (prependTotais)
+# 2 - Lista de ASes com prepend na prependOrigem (prependOrigem)
+# 3 - Lista de ASes com prepend prependIntermediario (prependIntermed)
+# 4 - Lista de ASes que fazem prepend nivel 1 (prepNivel1)
+# 5 - Lista de ASes que fazem prepend nivel 2 (prepNivel2)
+# 6 - Lista de ASes que fazem prepend nivel 3 (prepNivel3)
+# 7 - Lista de ASes que fazem prepend nivel 4 ou + (prepNivel4plus)
+# 8 - Lista de ASes unicos visualizados (asesTotais)
+# 9 - ASes com seus respectivos vizinhos listados (vizinhos)
